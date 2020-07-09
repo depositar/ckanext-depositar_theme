61d979ff0cb54d14f615838b1d34005d5c21ccb1
Sol Lee <u103133.u103135@gmail.com>
2020-07-09 10:51:22 +0800

    Add indicators for multi-value form fields

---
+++ ckanext/depositar_theme/fanstatic/scripts/modules/field-extra.js
@@ -0,0 +1,26 @@
+/* A module for adding extras to fields.
+*/
+this.ckan.module('field-extra', function (jQuery) {
+  return {
+    initialize: function () {
+      var label_name = this.el.data('label-name') || this.el.data('name');
+      var icon = this.el.data('icon');
+      var multiple = this.el.data('multiple');
+      var label = $("[for=field-" + label_name + "]");
+      var required = label.find(".control-required");
+
+      if (icon) {
+        label.prepend('<i class="fa fa-' + icon + '"></i> ');
+      }
+
+      if (multiple) {
+        $('<span> </span><span title="' + this._("This field accepts multiple values") + '" class="control-required">†</span>').appendTo(label);
+      }
+
+      if (required.length != 0) {
+        $("<span> </span>").appendTo(this.label);
+        required.appendTo(label);
+      }
+    }
+  }
+});
