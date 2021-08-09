d4ecd4a582428dd9e3f191d16e09a5abef813279
Sol Lee <u103133.u103135@gmail.com>
2021-08-09 23:43:31 +0800

    Bs4 overrides from ckanext-data-depositario

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const { src, watch, dest, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const less = require("gulp-less");
+const if_ = require("gulp-if");
+const sourcemaps = require("gulp-sourcemaps");
+
+const with_sourcemaps = () => !!process.env.DEBUG;
+
+const buildLess = () =>
+  src(__dirname + "/ckanext/depositar_theme/fanstatic/less/main.less", {"allowEmpty": true})
+    .pipe(if_(with_sourcemaps(), sourcemaps.init()))
+    .pipe(less())
+    .pipe(if_(with_sourcemaps(), sourcemaps.write()))
+    .pipe(dest(__dirname + "/ckanext/depositar_theme/fanstatic/styles/"));
+
+const buildScss = () =>
+  src(__dirname + "/ckanext/depositar_theme/fanstatic/scss/bootstrap-4.scss")
+    .pipe(if_(with_sourcemaps(), sourcemaps.init()))
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(if_(with_sourcemaps(), sourcemaps.write()))
+    .pipe(dest(__dirname + "/ckanext/depositar_theme/fanstatic/styles/"));
+
+const watchSource = () =>
+  watch([
+    __dirname + "/ckanext/depositar_theme/fanstatic/scss/**/*.scss",
+    __dirname + "/ckanext/depositar_theme/fanstatic/less/**/*.less"],
+    { ignoreInitial: false },
+    parallel(
+      buildLess,
+      buildScss
+    )
+  );
+
+const bootstrap = () =>
+  src(__dirname + "/node_modules/bootstrap/scss/**/*").pipe(
+    dest(__dirname + "/ckanext/depositar_theme/fanstatic/scss/vendor/bootstrap/")
+  )
+
+exports.build = parallel(
+  buildLess,
+  buildScss
+);
+exports.watch = watchSource;
+exports.updateVendorLibs = parallel(
+  bootstrap,
+);
