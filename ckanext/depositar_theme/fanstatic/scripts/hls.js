e810dbe85d1325c2aa10ed747c47a063d4a58b74
MonicaMigu <monica2014939@yahoo.com.tw>
2022-10-11 15:01:11 +0000

    Modify video path & Add english video

--- ckanext/depositar_theme/fanstatic/scripts/hls.js
+++ ckanext/depositar_theme/fanstatic/scripts/hls.js
@@ -2,7 +2,7 @@ let videoList = document.getElementsByClassName('video');
 let hls = Array.from(Array(3), (_, i) => new Hls());
 
 for (let i = 0; i < videoList.length; i++) {
-  let src = '/videos/' + videoList[i].getAttribute('id') + '.m3u8';
+  let src = '/videos/' + document.documentElement.lang + '/' + videoList[i].getAttribute('id') + '.m3u8';
   if (videoList[i].canPlayType('application/vnd.apple.mpegurl')) {
     videoList[i].src = src;
   } else if (Hls.isSupported()) {
