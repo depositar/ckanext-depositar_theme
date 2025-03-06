5d0284b14945449eea81f140ace01886af0c47e2
tychuang <tychuang@citi.sinica.edu.tw>
2025-03-06 16:10:15 +0800

    Migrate less to scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { src, watch, dest, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const less = require("gulp-less");
+//const less = require("gulp-less");
 const if_ = require("gulp-if");
 const sourcemaps = require("gulp-sourcemaps");
 
@@ -9,10 +9,10 @@ const with_sourcemaps = () => !!process.env.DEBUG;
 
 const buildLess = () =>
   src([
-    __dirname + "/ckanext/depositar_theme/fanstatic/less/main/main.less"
+    __dirname + "/ckanext/depositar_theme/fanstatic/less/main/main.scss"
   ])
     .pipe(if_(with_sourcemaps(), sourcemaps.init()))
-    .pipe(less({ math: 'always' }))
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(if_(with_sourcemaps(), sourcemaps.write()))
     .pipe(dest(__dirname + "/ckanext/depositar_theme/fanstatic/styles/"));
 
@@ -33,7 +33,7 @@ const bootstrapScss = () =>
 const watchSource = () =>
   watch([
     __dirname + "/ckanext/depositar_theme/fanstatic/scss/**/*.scss",
-    __dirname + "/ckanext/depositar_theme/fanstatic/less/**/*.less"],
+    __dirname + "/ckanext/depositar_theme/fanstatic/less/**/*.scss"],
     { ignoreInitial: false },
     parallel(
       buildLess,
