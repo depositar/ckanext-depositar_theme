a17669123c021aaf6a972a0627a100d362ab7187
tychuang1211 <33109379+tychuang1211@users.noreply.github.com>
2025-03-27 11:59:39 +0800

    Apply suggestions from code review
    Co-authored-by: Sol Lee <u103133.u103135@gmail.com>

--- ckanext/depositar_theme/fanstatic/scripts/alert.js
+++ ckanext/depositar_theme/fanstatic/scripts/alert.js
@@ -1,4 +1,4 @@
 (function() {
   let alertEl = document.getElementsByClassName('alert-dismissible');
-  alertEl.forEach(e => e.innerHTML += '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>');
+  alertEl.forEach(e => e.innerHTML += '<button type="button" class="btn-close close" data-bs-dismiss="alert" aria-label="Close"></button>');
 })();
